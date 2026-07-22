cb0d771ffd4a9ff059a57700b50a8f44bd8f6cec
ScrapeSmith <Ahmad021Rehman@gmail.com>
2026-07-22 18:47:02 +0500

    Update articles.ts

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -20,6 +20,7 @@ export type Article = {
 };
 
 import { articlesBatch2 } from "./articles-batch2";
+import { articlesBatch3 } from "./articles-batch3";
 
 const batch1: Article[] = [
   {
@@ -434,7 +435,7 @@ const batch1: Article[] = [
   },
 ];
 
-export const articles: Article[] = [...batch1, ...articlesBatch2];
+export const articles: Article[] = [...batch1, ...articlesBatch2, ...articlesBatch3];
 
 export function getArticle(slug: string) {
   return articles.find((a) => a.slug === slug);
